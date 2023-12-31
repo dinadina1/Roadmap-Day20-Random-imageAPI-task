f602a792cecf648f1667ed23d4da652cf4563693
dinadina1 <dinad9355@gmail.com>
2023-12-31 16:08:32 +0530

    last commit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const data = fetch(`https:/dog.ceo/api/breeds/image/random/${count}`);
 data
   .then((response) => response.json())
   .then((dogImage) => {
-    for (let i = 0; i < dogImage.message.length; i++) {
+    for (let i = 0; i < count; i++) {
       document.getElementById("cards").innerHTML += `
         <img class="col-lg-4 col-sm-12 col-md-6 card card-img" src="${dogImage.message[i]}" alt="dog-image">`;
     }
